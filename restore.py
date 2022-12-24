3c11dd6f353e2d29d84fbacb9a5dea68bf54d548
Dan <dansonlinepresence@gmail.com>
2022-12-23 19:50:24 -0500

    restore

---
+++ restore.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('db_name')
+parser.add_argument('dump_path')
+parser.add_argument('--docker-container')
+args = parser.parse_args()
+
+def invoke(invocation, **kwargs):
+    kwargs.setdefault('check', True)
+    return subprocess.run(invocation.split(), **kwargs)
+
+invocation = f'psql -d {args.db_name}'
+
+if args.docker_container:
+    invocation = 'docker exec -i -u postgres {args.docker_container} {invocation}'
+
+with open(args.dump_path) as f:
+    invoke(invocation, stdin=f)
